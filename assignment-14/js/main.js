1ea0ae0b3f6a5dee15085cccb1c1b39e72c80126
Bethany Heisey <bethany.heisey@umontana.edu>
2023-12-07 12:46:30 -0700

    Update main.js

--- assignment-14/js/main.js
+++ assignment-14/js/main.js
@@ -2,6 +2,6 @@ var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
 
-ctx.fillStyle = "red"
-ctx.fillRect(0, 0, 62.5, 62.5)
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 62.5, 62.5);
 
