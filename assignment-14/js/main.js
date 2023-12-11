a323353dd5472ed60f8fcf2aae1a33c83f163b04
Bethany Heisey <bethany.heisey@umontana.edu>
2023-12-10 21:27:49 -0700

    Update main.js

--- assignment-14/js/main.js
+++ assignment-14/js/main.js
@@ -8,13 +8,16 @@ ctx.fillRect(112.5, 0, 62.5, 62.5);
 ctx.fillRect(225, 0, 62.5, 62.5);
 ctx.fillRect(337.5, 0, 62.5, 62.5);
 
+document.addEventListener("DOMContentLoaded", function() {
 
-var c2 = document.getElementById("myCanvas2");
-var ctx2 = c2.getContext("2d");
+    var c2 = document.getElementById("myCanvas2");
+    var ctx2 = c2.getContext("2d");
 
 
-ctx2.fillStyle = "red";
-ctx2.fillRect(0, 0, 337.5, 337.5);
-ctx2.fillRect(387.5, 0, 337.5, 337.5);
-ctx2.fillRect(775, 0, 337.5, 337.5);
-ctx2.fillRect(1162.5, 0, 337.5, 337.5);
+    ctx2.fillStyle = "red";
+    ctx2.fillRect(0, 0, 337.5, 337.5);
+    ctx2.fillRect(387.5, 0, 337.5, 337.5);
+    ctx2.fillRect(775, 0, 337.5, 337.5);
+    ctx2.fillRect(1162.5, 0, 337.5, 337.5);
+    
+});
